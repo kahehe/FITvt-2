0231e42ed174711bcb2c1f2727970659f176f0d7
shaunnmui97 <e0325692@u.nus.edu>
2020-11-03 21:48:29 +0800

    removed firebase credentials

--- fitvt/src/main.js
+++ fitvt/src/main.js
@@ -10,14 +10,14 @@ Vue.config.productionTip = false;
 
  // Your web app's Firebase configuration
  var firebaseConfig = {
-    apiKey: "AIzaSyDIEA01msH8_f6PB3eIP6WOtyX5Oovz_sk",
-    authDomain: "fitvt-4d8bd.firebaseapp.com",
-    databaseURL: "https://fitvt-4d8bd.firebaseio.com",
-    projectId: "fitvt-4d8bd",
-    storageBucket: "fitvt-4d8bd.appspot.com",
-    messagingSenderId: "114620864183",
-    appId: "1:114620864183:web:50fbed92a03dd34a7d9f32",
-    measurementId: "G-F5PWXHRGPM" 
+    apiKey: 
+    authDomain: 
+    databaseURL: 
+    projectId: 
+    storageBucket: 
+    messagingSenderId: 
+    appId: 
+    measurementId: 
   };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
