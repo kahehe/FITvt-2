f39b05d41d13e8d424b1a167ec6a364c0816f2f6
jiayi-lim <65161176+jiayi-lim@users.noreply.github.com>
2020-11-04 21:43:37 +0800

    start on create workout component

---
+++ fitvt/src/views/secret/create-workout.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="create-workout main">
+    <Spinner v-if="loading" />
+    <aside class="left"><SideBar /></aside>
+    <main>
+      <sNavBar />
+      <p class="error" v-if="error">{{ error }}</p>
+      <section id="workout-title">
+        <h3>Title of workout:</h3>
+        <br />
+        <br />
+        <input type="text" id="title" v-model="title" />
+      </section>
+    </main>
+    <aside class="right">
+      <CalendarChart />
+    </aside>
+  </div>
+</template>
+
+<script>
+import SideBar from "@/components/secret/SideBar";
+import CalendarChart from "@/components/secret/Calendar-Chart";
+import sNavBar from "@/components/secret/NavBar";
+import Spinner from "@/components/Spinner";
+//import firebase from "firebase/app";
+export default {
+  components: {
+    SideBar,
+    CalendarChart,
+    sNavBar,
+    Spinner,
+  },
+  data() {
+    return {
+      title: "",
+      description: "",
+      loading: false,
+      error: "",
+    };
+  },
+};
+</script>
+
+<style scoped>
+</style>
